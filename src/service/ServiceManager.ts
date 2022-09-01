306d794b45bbdda63719445a8729e583de7d551d
Andrew Evers <aevers@brandevolutionco.com>
2022-09-01 17:27:34 -0500

    added ServiceManager

---
+++ src/service/ServiceManager.ts
@@ -0,0 +1,41 @@
+import { EntityManager } from '../repository'
+import AccountService from './AccountService'
+import PositionService from './PositionService'
+import TriggerService from './TriggerService'
+
+interface ServiceManagerArgs {
+	entityManager: EntityManager
+	services?: {
+		account: AccountService
+		trigger: TriggerService
+		position: PositionService
+	}
+}
+
+type ServicesByName = {
+	account: AccountService
+	trigger: TriggerService
+	position: PositionService
+}
+
+class ServiceManager {
+	private readonly services: {
+		[K in keyof ServicesByName]: ServicesByName[K]
+	}
+
+	constructor(args: ServiceManagerArgs) {
+		const { entityManager, services } = args
+		const positionService = services?.position || new PositionService({ entityManager })
+		const accountService = services?.account || new AccountService({ entityManager, positionService })
+		const triggerService = services?.trigger || new TriggerService({ entityManager, accountService })
+		this.services = {
+			account: accountService,
+			trigger: triggerService,
+			position: positionService
+		}
+	}
+
+	public getService<T extends keyof ServicesByName>(serviceName: T): ServicesByName[T] {
+		return this.services[serviceName]
+	}
+}
